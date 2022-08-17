caa4cd9a7c578d90690e2243b71dfe5a046b50fc
ItsFenil <102537116+ItsFenil@users.noreply.github.com>
2022-08-17 14:12:44 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
       </ul>
       <div className="footer__socials">
       <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      <a href="https://github.com/ItsFenil" target="_blank" rel="noreferrer" ><FaGithub /></a>
       </div>
     </footer>
   )
