fe9483336d875110d392500a56ccee84862a2d26
ItsFenil <102537116+ItsFenil@users.noreply.github.com>
2023-06-19 00:50:19 +0530

    first commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -20,7 +20,7 @@ const Intro = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>4 projects completed as Freelancer</small>
+              <small>6 projects completed as Freelancer</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
